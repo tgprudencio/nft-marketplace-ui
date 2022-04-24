3be929e11957b6f6211c1dda7d25f5801ed037f9
Thiago Gonçalves Prudêncio <thiago.gprudencio@gmail.com>
2022-04-24 17:50:47 -0300

    feat: add header component and Details screen

--- components/index.js
+++ components/index.js
@@ -2,8 +2,12 @@ import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
 import { CircleButton, RectButton } from "./Button";
+import DetailsDesc from "./DetailsDesc";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
 
 export {
     HomeHeader, FocusedStatusBar, NFTCard,
-    CircleButton, RectButton
+    CircleButton, RectButton, DetailsDesc,
+    DetailsBid, SubInfo
 }
