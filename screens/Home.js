ca28ec99674f1a2c86e9338d2da00796b6d4253b
Thiago Gonçalves Prudêncio <thiago.gprudencio@gmail.com>
2022-04-24 21:34:54 -0300

    feat: add searchBar function

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,36 @@
 import { useState } from 'react';
-import { FlatList, SafeAreaView, Text, View } from 'react-native'
+import { FlatList, SafeAreaView, View } from 'react-native'
 import { COLORS, NFTData } from '../constants';
-
 import  { FocusedStatusBar, HomeHeader, NFTCard } from '../components';
 
 const Home = () => {
+  const [nftData, setNftData] = useState(NFTData);
+
+  const handleSearch = (value) => {
+    if (!value.length) return setNftData(NFTData);
+
+    const filteredData = NFTData.filter((item) => 
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    if (filteredData.length) {
+      setNftData(filteredData);
+    } else {
+      setNftData(NFTData);
+    }
+  }
+
   return (
     <SafeAreaView style = {{ flex: 1 }}>
       <FocusedStatusBar background = { COLORS.primary } />
       <View style = {{ flex: 1 }}>
         <View style = {{ zIndex: 0 }}>
           <FlatList 
-            data = { NFTData }
+            data = { nftData }
             renderItem = { ({ item }) => <NFTCard data = { item } /> }
             keyExtractor = { (item) => item.id }
             showsVerticalScrollIndicator = { false }
-            ListHeaderComponent = { <HomeHeader /> }
+            ListHeaderComponent = { <HomeHeader onSearch = { handleSearch } /> }
           />
         </View>
         <View style = {{ 
